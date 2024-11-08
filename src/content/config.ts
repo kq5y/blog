46e2e53a0da13ef18b05fe2c8eb569e1bcef1a4d
kq5y <tkser.edu@gmail.com>
2024-11-08 20:00:51 +0900

    add: memo mode

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,16 @@ const blogCollection = defineCollection({
   }),
 });
 
+const memoCollection = defineCollection({
+  schema: z.object({
+    title: z.string().max(100).min(1),
+    tags: z.array(z.string()).max(5).min(1),
+    date: z.string().regex(/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$/),
+    hidden: z.boolean().optional(),
+  }),
+});
+
 export const collections = {
   blog: blogCollection,
+  memo: memoCollection,
 };
---
+++ src/lib/article.ts
@@ -0,0 +1,42 @@
+import { getCollection, type ContentEntryMap } from "astro:content";
+
+export interface Article {
+  title: string;
+  tags: string[];
+  date: string;
+  hidden?: boolean;
+  slug: string;
+  body: string;
+}
+
+export async function getAllArticles(
+  cat: keyof ContentEntryMap,
+  showHidden: boolean = false
+): Promise<Article[]> {
+  const articles = await getCollection(cat);
+  return articles
+    .filter((article) => showHidden || !(article.data.hidden === true))
+    .map((article) => ({
+      ...article.data,
+      slug: article.slug,
+      body: article.body,
+    }))
+    .sort((a, z) => new Date(z.date).getTime() - new Date(a.date).getTime());
+}
+
+export function convertArticles(articles: Article[], cat: string) {
+  return articles.map((article) => {
+    return {
+      params: {
+        cat: cat,
+        slug: article.slug,
+      },
+      props: {
+        title: article.title,
+        tags: article.tags,
+        date: article.date,
+        body: article.body,
+      },
+    };
+  });
+}
