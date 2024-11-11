8bc69ea5b51163959552e471a77a3c207628128c
kq5y <tkser.edu@gmail.com>
2024-11-11 23:59:00 +0900

    fix: format

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import remarkLinkCard from "remark-link-card";
-import remarkMath from "remark-math";
-import rehypeRaw from "rehype-raw";
-import rehypeExternalLinks from "rehype-external-links";
-import rehypeKatex from "rehype-katex";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import rehypeExpressiveCode from "rehype-expressive-code";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeKatex from "rehype-katex";
+import rehypeRaw from "rehype-raw";
+import remarkLinkCard from "remark-link-card";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   integrations: [tailwind()],
