8c9eef53b2b12339a130f9945acfdfacf49a55b2
kq5y <tkser.edu@gmail.com>
2024-11-11 17:47:52 +0900

    add: external link

--- astro.config.ts
+++ astro.config.ts
@@ -4,13 +4,22 @@ import tailwind from "@astrojs/tailwind";
 
 import remarkLinkCard from "remark-link-card";
 import remarkMath from "remark-math";
+import rehypeRaw from "rehype-raw";
+import rehypeExternalLinks from "rehype-external-links";
 import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   integrations: [tailwind()],
   markdown: {
-    remarkPlugins: [remarkLinkCard, remarkMath],
-    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [[remarkLinkCard, { shortenUrl: true }], remarkMath],
+    rehypePlugins: [
+      rehypeRaw,
+      [
+        rehypeExternalLinks,
+        { target: "_blank", rel: ["noopener", "noreferrer"] },
+      ],
+      rehypeKatex,
+    ],
     remarkRehype: {
       footnoteLabel: "脚注",
     },
