14151dbd4a68ca2770fbb79bd995ecf585531a43
kq5y <tkser.edu@gmail.com>
2024-12-03 14:40:44 +0900

    switch from eslint to biome

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginAstro from "eslint-plugin-astro";
-
-export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
-  eslintConfigPrettier,
-];
